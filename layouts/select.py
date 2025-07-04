9eedcde516890f82f6037af01c05a083067abe07
vicoumich <vicou1804@gmail.com>
2025-07-04 11:41:28 +0200

    Default select values added

--- layouts/select.py
+++ layouts/select.py
@@ -15,6 +15,7 @@ def get_layout():
 
     # Fréquences de DownSampling
     ds_freqs = [1024, 512, 256, 128, 64]
+    prefix = '1-' if channels[0][:2] == '1-' or channels[1][:2] == '1-' else ''
 
     return html.Div([
         html.H2(f"Sélectionner les canaux intéressants :{channels}"),
@@ -22,6 +23,7 @@ def get_layout():
         html.Label("ECG (1 ou 2) :"),
         dcc.Dropdown(
             id='ecg-dropdown',
+            value= [prefix + 'EXG1-0', prefix + 'EXG2-0'],
             options=[{'label': ch, 'value': ch} for ch in channels],
             multi=True
         ),
@@ -31,6 +33,7 @@ def get_layout():
         html.Label("Respiration :"),
         dcc.Dropdown(
             id='resp-dropdown',
+            value= prefix + 'Resp',
             options=[{'label': ch, 'value': ch} for ch in channels],
             multi=False
         ),
@@ -40,6 +43,7 @@ def get_layout():
         html.Label("Status :"),
         dcc.Dropdown(
             id='status-dropdown',
+            value='Status',
             options=[{'label': ch, 'value': ch} for ch in channels],
             multi=False
         ),
@@ -49,6 +53,7 @@ def get_layout():
         html.Label("Micro :"),
         dcc.Dropdown(
             id='micro-dropdown',
+            value= prefix + 'micro',
             options=[{'label': ch, 'value': ch} for ch in channels],
             multi=False
         ),
@@ -58,6 +63,7 @@ def get_layout():
         html.Label("GSR :"),
         dcc.Dropdown(
             id='gsr-dropdown',
+            value= prefix + 'GSR1',
             options=[{'label': ch, 'value': ch} for ch in channels],
             multi=False
         ),
@@ -68,6 +74,7 @@ def get_layout():
         dcc.Dropdown(
             id='downsampling-dropdown',
             options=[{'label': ds_freq, 'value': ds_freq} for ds_freq in ds_freqs],
+            value=256,
             multi=False
         ),
 
